d99193b110da9be6db9156841cdf068ff76f3d42
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2022-03-07 22:42:10 +0100

    Change routing method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { APP_BASE_HREF } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
@@ -31,8 +32,11 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { useHash: true, initialNavigation: "enabledBlocking" })
+    RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue : "/" }
+  ]
 })
 export class AppRoutingModule { }
